2019cef9791f8d5ccbf611b3b427ab12f3f3b811
Jordan Bice <jmbice@gmail.com>
2018-05-06 20:23:14 -0700

    Complete functions except uniq, map, pluck, reduce

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val
   };
 
   /**
@@ -37,6 +38,13 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n === undefined) {
+      return array[array.length-1]
+    } else if (n > array.length){
+      return array;
+    } else {
+      return array.slice(array.length-n)
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +53,21 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (typeof collection === 'object' && Array.isArray(collection)) {
+      for (var j = 0; j < collection.length; j++){
+        var index = j;
+        iterator(collection[j], index, collection)
+      }
+    } else if (typeof collection === 'object'){
+      for (var keys in collection){
+        iterator(collection[keys], keys, collection)
+      }
+    } else {
+      console.log('Jordan made a mistake. Kill him!');
+    }
+
+
+
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,16 +89,36 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var passingArray = [];
+    for (var j = 0; j < collection.length; j++){
+      test(collection[j]) === true ? passingArray.push(collection[j]) : null;
+    }
+    return passingArray
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var passingArray = [];
+    for (var j = 0; j < collection.length; j++){
+      test(collection[j]) === false ? passingArray.push(collection[j]) : null;
+    }
+    return passingArray
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+    var uniqArray = [];
+
+    for (var j = 0; j < array.length; j++) {
+      if (isSorted){
+        uniqArray[uniqArray.length - 1] !== array[j] ? uniqArray.push(array[j]) : null;
+      } else {
+        uniqArray.includes(array[j]) ? null : uniqArray.push(array[j]);
+      }
+    }
+    return uniqArray
   };
 
 
@@ -107,19 +150,19 @@
   // Reduces an array or object to a single value by repetitively calling
   // iterator(accumulator, item) for each item. accumulator should be
   // the return value of the previous iterator call.
-  //  
+  //
   // You can pass in a starting value for the accumulator as the third argument
   // to reduce. If no starting value is passed, the first element is used as
   // the accumulator, and is never passed to the iterator. In other words, in
   // the case where a starting value is not passed, the iterator is not invoked
   // until the second element, with the first element as its second argument.
-  //  
+  //
   // Example:
   //   var numbers = [1,2,3];
   //   var sum = _.reduce(numbers, function(total, number){
   //     return total + number;
   //   }, 0); // should be 6
-  //  
+  //
   //   var identity = _.reduce([5], function(total, number){
   //     return total + number * number;
   //   }); // should be 5, regardless of the iterator function passed in
